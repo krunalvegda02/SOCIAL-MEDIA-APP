37caa1d699cd2d6daa75c30c6944a3d14d975e78
Vegda Krunal <Krunal.vegda02@gmail.com>
2024-07-24 20:49:29 +0530

    User provider issues

--- lib/Providers/User_provider.dart
+++ lib/Providers/User_provider.dart
@@ -1,18 +1,1 @@
-import 'package:flutter/material.dart';
-import 'package:sm/Models/user.dart';
-import 'package:sm/Resources/auth_method.dart';
 
-class UserProvider with ChangeNotifier {
-  User? _user;
-  final AuthMethod _authMethod = AuthMethod();
-
-  User get getUser => _user!;
-
-  Future<void> refreshUser() async {
-    User user = await _authMethod.getUserDetails();
-    _user = user;
-    //NOTIFY LISTENER NOTIFY ALL THE LESTENERS TO THEIR PROVIDER THAT THE
-    //DATA OF OUR GLOBAL VARIABLE USER HAS CHANGED AND YOU NEED TO UPDATE YOUR VALUE
-    notifyListeners();
-  }
-}
